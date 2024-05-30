9467ef51166ff78e3f92979f6224c4acf06acad7
ThASaNdGgUy <TheTrueRealKarKar@gmail.com>
2024-05-30 16:05:10 -0400

    Create InfiniteJumpMod.js

---
+++ InfiniteJumpMod.js
@@ -0,0 +1,26 @@
+Infinite Jump Mod
+
+// ==UserScript==
+// @name         Boxel 3D Infinite Jump Mod
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Allows the player to jump infinitely with no cooldown
+// @author       ThASaNdGgUy
+// @match        *dopplercreative.com/test/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function modifyJump() {
+        if (!app || !app.player) return;
+
+        app.player.allowJump = true; 
+    }
+
+    addUpdateFunction(modifyJump);
+
+    addModToList("Infinite Jump");
+})();
