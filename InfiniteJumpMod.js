7fdc0cd595cd703296108b467756f21ed4d3f2bc
ThASaNdGgUy <TheTrueRealKarKar@gmail.com>
2024-11-25 23:58:52 -0500

    self explanatory ig

--- InfiniteJumpMod.js
+++ InfiniteJumpMod.js
@@ -2,7 +2,7 @@
 // @name         Infinite Jump
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Allows the player to jump infinitely with no cooldown
+// @description  Allows the player to jump infinitely with no cooldown!
 // @author       ThASaNdGgUy
 // @match        *dopplercreative.com/test/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
