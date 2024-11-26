ebce25666ca8afa330b892d9b955f3cfb36d6d7a
ThASaNdGgUy <TheTrueRealKarKar@gmail.com>
2024-11-25 23:48:45 -0500

    KillButtonMod.js

---
+++ KillButtonMod.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Kill Button
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Press K To Kill Yourself
+// @author       ThASaNdGgUy
+// @match        *dopplercreative.com/test/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function killPlayer(event) {
+        if (event.key === 'k' || event.key === 'K') {
+            app.player.kill();
+        }
+    }
+
+    document.addEventListener('keypress', killPlayer);
+
+    addModToList("Kill Button");
+})();
